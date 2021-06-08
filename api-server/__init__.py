9b08e4f0d13b69fb41c60a330411d7df30e139ad
Alexandre Andrade <aleandrade@gmail.com>
2021-06-08 23:05:20 +1000

    feat: 🎸 add initial API

---
+++ api-server/__init__.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Image depth endpoint
+    @app.route('/generate-depth')
+    def depth():
+        return 'Hello, World!'
+
+    return app
