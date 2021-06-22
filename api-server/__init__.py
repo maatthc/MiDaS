1aac961b71cd320798f97a12215d785d18b6f499
Alexandre Andrade <aleandrade@gmail.com>
2021-06-22 13:30:35 +1000

    feat: 🎸 check if image exists

--- api-server/__init__.py
+++ api-server/__init__.py
@@ -40,23 +40,27 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    def inputImageName(imageName):
+        return app.config['IMAGE_INPUT'] + imageName + '.jpg'
+
     # Image depth endpoint
     @app.route('/generate-depth', methods=(['GET']))
     def depth():
         imageUrl = request.args.get('url')
         splitUrl = parse.urlsplit(imageUrl)
         imageName = splitUrl.path.split('/')[-1].split('.')[0]
-        img_data = requests.get(
-            imageUrl, headers=app.config['HEADERS'])
-        if img_data.status_code != 200:
-            app.logger.error(
-                f'Unexpected return code ({img_data.status_code}) when downloading asset: {imageUrl}')
-            abort(500)
-        with open(app.config['IMAGE_INPUT'] + imageName + '.jpg', 'wb') as handler:
-            handler.write(img_data.content)
-        process(imageName, app.config['IMAGE_INPUT'], app.config['OPTIMIZE'],
-                app.config['DEVICE'], app.config['MODEL'], app.config['IMAGE_OUTPUT'], app.config['TRANSFORM'])
-        os.remove(app.config['IMAGE_INPUT'] + imageName + '.jpg')
+        if not os.path.isfile(inputImageName(imageName)):
+            img_data = requests.get(
+                imageUrl, headers=app.config['HEADERS'])
+            if img_data.status_code != 200:
+                app.logger.error(
+                    f'Unexpected return code ({img_data.status_code}) when downloading asset: {imageUrl}')
+                abort(404)
+            with open(inputImageName(imageName), 'wb') as handler:
+                handler.write(img_data.content)
+            process(imageName, app.config['IMAGE_INPUT'], app.config['OPTIMIZE'],
+                    app.config['DEVICE'], app.config['MODEL'], app.config['IMAGE_OUTPUT'], app.config['TRANSFORM'])
+            # os.remove(inputImageName(imageName))
         return send_from_directory(app.config['IMAGE_OUTPUT'], imageName + '.png', mimetype='image/png')
 
     return app
